f6b80bdc26748ad7d74902674686d5701920760b
Kritish Mohapatra <kritishmohapatra06norisk@gmail.com>
2026-03-01 16:13:06 +0530

    Update LED print statements and speed up loop
    Updated print statements to reflect version 1.0.1 and reduced sleep duration.

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ led = Pin("LED", Pin.OUT)
 
 while True:
     led.on()
-    print("LED ON - Version 1.0.0")
-    time.sleep(1)
+    print("LED ON - Version 1.0.1")  # ← changed
+    time.sleep(0.2)                   # ← faster!
     led.off()
-    print("LED OFF - Version 1.0.0")
-    time.sleep(1)
+    print("LED OFF - Version 1.0.1") # ← changed
+    time.sleep(0.2)                   # ← faster!
